54644377ad55dacc50d554465dcbbf0b90bf4198
Chinzzii <chinmay.singhania@gmail.com>
2024-11-17 21:04:42 -0500

    Fix

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,17 +1,23 @@
 package main
 
-import(
+import (
 	"log"
 	"net/http"
+
+	"github.com/Chinzzii/CRUD-API-with-MySQL-and-Go/pkg/routes"
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/Chinzzii/CRUD-API-with-MySQL-and-Go/pkg/routes"
 )
 
-func main(){
+func main() {
 	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome to the Bookstore API! Use /book/ to access the API endpoints."))
+	})
+
 	routes.RegisterBookStoreRoutes(r)
-	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
-	
+
 }
